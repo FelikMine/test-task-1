4bdc99b18cc8b40234fe18f513ff3cf25415a2eb
FelikMine <112383831+FelikMine@users.noreply.github.com>
2024-08-09 13:35:40 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const aside = document.getElementsByTagName('aside')[0];
+const leftBtns = aside.getElementsByTagName('a'); 
+const hide_menu = document.querySelector(".hide-menu");
+// console.log(leftBtns);
+hide_menu.addEventListener("click", closeNav);
+// for (let i = 0; i < leftBtns.length; i++) {
+//     const el = leftBtns[i];
+//     el.addEventListener("click", closeNav);
+// }
+function openNav() {
+    document.querySelector("aside")
+        .style.width = "300px";
+    document.getElementById("contentArea")
+        .style.marginLeft = "300px";
+}
+
+function closeNav() {
+    // aside.style.width = "0";
+    console.log(aside.children);
+    aside.style.transition = "opacity 1s"
+    aside.style.opacity = "0";
+    // document.getElementById("contentArea")
+    //     .style.marginLeft = "0";
+}
